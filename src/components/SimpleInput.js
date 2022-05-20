3acd5695544349cb66aff75d0e9f9d888d65ddde
dsampath <dsampath@wiley.com>
2022-05-20 14:48:56 +0530

    handled form validation using useEffect

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,19 +1,34 @@
-import {useState, useRef} from "react";
+import {useState, useRef, useEffect} from "react";
 
 const SimpleInput = (props) => {
 
     const nameInputRef = useRef();//the last value will be returned
     const[enteredName, setEnteredName] = useState('');
+    const[enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+    const[enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+    useEffect(()=>{
+        if(enteredNameIsValid){
+            console.log('Name Input is valid');
+        }
+    }, [enteredNameIsValid]);
 
     const formSubmissionHandler = event => {
         event.preventDefault();//this is to prevent reloading
+
+        setEnteredNameTouched(true);
+
+        if(enteredName === ''){
+            setEnteredNameIsValid(false);
+            return;
+        }
+        setEnteredNameIsValid(true);
         console.log(enteredName);
         const enteredValue = nameInputRef.current.value;
         console.log(enteredValue);
         setEnteredName('');//this is to empty the input field after the form is submitted
         //same can be done by useRef also. but its not nice as useState
-        // nameInputRef.current.value = '';  <-here we directly manipulating the dom here. which is not suppose to do
-
+        // nameInputRef.current.value = '';  <-here we directly manipulating the dom here. which is not ideal to do
     }
 
     const btnOnClickHandler = () => {
@@ -25,11 +40,16 @@ const SimpleInput = (props) => {
         // console.log(enteredName);
     }
 
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+
     return (
         <form onSubmit={formSubmissionHandler}>
-            <div className='form-control'>
+            <div className={nameInputClasses}>
                 <label htmlFor='name'>Your Name</label>
                 <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName}/>
+                {nameInputIsInvalid && <p className="error-text">name must not be empty</p>}
             </div>
             <div className="form-actions">
                 <button onClick={btnOnClickHandler}>Submit</button>
